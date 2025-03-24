99cf72209c904e959d8e0c5d85def42640183e76
landon <lagic@ufl.edu>
2025-03-24 10:43:23 -0400

    added a sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login_Page from "./components/Login_Page";
 import Dashboard from "./components/Dashboard";
+import Sign_Up from "./components/Sign_Up"; // Import the sign-up page
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login_Page />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Sign_Up />} />
         <Route path="/" element={<Login_Page />} />
       </Routes>
     </Router>
